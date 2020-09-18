cae85a332aa056d16569646c5a0d2e01f45b1560
Lucas <lucaoliveira57@gmail.com>
2020-09-17 23:39:20 -0300

    EditLogModal & Technician modal components

--- src/components/logs/EditLogModal.js
+++ src/components/logs/EditLogModal.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import M from 'materialize-css/dist/js/materialize.min.js';
 
-const AddLogModal = () => {
+const EditLogModal = () => {
   const [message, setMessage] = useState('');
   const [attention, setAttention] = useState(false);
   const [tech, setTech] = useState('');
@@ -20,7 +20,7 @@ const AddLogModal = () => {
   };
 
   return (
-    <div id="add-log-modal" className="modal" style={modalStyle}>
+    <div id="edit-log-modal" className="modal" style={modalStyle}>
       <div className="modal-content">
         <h4>Enter System Log</h4>
         <div className="row">
@@ -74,7 +74,7 @@ const AddLogModal = () => {
       </div>
       <div className="modal-footer">
         <a
-          href="#!"
+          href="#1"
           onClick={onSubmit}
           className="modal-close waves-effect blue waves-light btn"
         >
@@ -90,4 +90,4 @@ const modalStyle = {
   height: '75%',
 };
 
-export default AddLogModal;
+export default EditLogModal;
